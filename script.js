84f3cf6cf2c2f0e8739685e3ed88252f65f06850
JULIAN HAZARD <hazardjulian74@gmail.com>
2024-12-09 10:55:30 +0100

    creation atelier wilders

---
+++ script.js
@@ -0,0 +1,18 @@
+import chalk from 'chalk';
+
+
+
+const lisa = "Lisa";
+const julien= "Julien";
+const maxime = "Maxime";
+const théophile = "Théophile";
+
+console.log (
+    chalk.green(lisa),
+    chalk.red(julien),
+    chalk.blue(maxime),
+    chalk.magenta(théophile),
+);
+
+
+
